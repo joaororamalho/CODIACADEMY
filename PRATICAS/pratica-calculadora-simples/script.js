641c6bdac4486778c463981ce0c04465ac0d34f1
devjoaoricardo <devjoaororamalho@gmail.com>
2025-05-24 11:05:01 -0300

    update script.js

--- PRATICAS/pratica-calculadora-simples/script.js
+++ PRATICAS/pratica-calculadora-simples/script.js
@@ -3,24 +3,42 @@ function calcular(operador) {
     let n2 = parseFloat(document.getElementById('num2').value);
     let resultado;
 
-    if (operador === '+') {
-        resultado = n1 + n2;
-    }
-    else if (operador === '-') {
-        resultado = n1 - n2;
-    }
-    else if (operador === '*') {
-        resultado = n1 * n2;
-    }
-    else if (operador === '/') {
-        if (n2 !== 0) {
-            resultado = n1 / n2;
-        } else {
-            resultado = 'Erro: divisão por zero';
-        }
-    }
-    else {
-        resultado = 'Operação inválida';
+    // if (operador === '+') {
+    //     resultado = n1 + n2;
+    // }
+    // else if (operador === '-') {
+    //     resultado = n1 - n2;
+    // }
+    // else if (operador === '*') {
+    //     resultado = n1 * n2;
+    // }
+    // else if (operador === '/') {
+    //     if (n2 !== 0) {
+    //         resultado = n1 / n2;
+    //     } else {
+    //         resultado = 'Erro: divisão por zero';
+    //     }
+    // }
+    // else {
+    //     resultado = 'Operação inválida';
+    // }
+
+
+    switch (operador) {
+        case '+':
+            resultado = n1 + n2;
+            break;
+        case '-':
+            resultado = n1 - n2;
+            break;
+        case '*':
+            resultado = n1 * n2;
+            break;
+        case '/':
+            resultado = (n2 !== 0 ? resultado = n1 / n2 : 'Erro: divisão por zero');
+            break;
+        default:
+            document.getElementById('operacao').innerText = 'Operação inválida';
     }
 
     document.getElementById('result').innerText = resultado;
